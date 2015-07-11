530e22ed4af3add697b12ec711ac5e1b21a2bc6a
xphong <xphong@gmail.com>
2015-07-11 09:05:44 -0400

    Add HelloWorld API

---
+++ api/controllers/HelloWorldController.js
@@ -0,0 +1,13 @@
+/**
+ * HelloWorldController
+ *
+ * @description :: Server-side logic for managing Helloworlds
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	index: function(req, res) {
+    res.send('Hello World');
+  }
+};
+
